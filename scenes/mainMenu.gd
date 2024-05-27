d075dd8c7dadcc3638499b0d59041f08c85f53ba
pt80 <45863938+pt80@users.noreply.github.com>
2024-05-27 18:13:48 -0400

    added char select

--- scenes/mainMenu.gd
+++ scenes/mainMenu.gd
@@ -1,15 +1,15 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_play_pressed():
 	print('yo DJ play dat shit')
+	get_tree().change_scene_to_file("res://scenes/characterSelect.tscn")
+
+
+func _on_options_pressed():
+	print('show me you options')
+
+
+func _on_exit_pressed():
+	print('bye btye')
+	get_tree().quit()
